63906ae72886fcc12c31d97d5c4d4486939f4956
flor condori <condorihuamanif@gmail.com>
2017-10-01 02:05:28 -0500

    agregando una tarea

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-app.js
+(function(){
+	var app = angular.module('todoApp', []);
+
+	app.controller('TodoController', function(){
+		this.todos = [];
+		this.todo = {};
+
+		this.addTodo = function(){
+			console.log(this.todos);
+			this.todo.completed = false;
+			this.todo.date = new Date();
+			this.todos.push(this.todo);
+			this.todo = {};
+		};
+	});
+
+})();
